a75fc5e143d29899d20854ff4cee3f02b166f2b1
NAD <lualua0909@gmail.com>
2019-08-30 15:58:27 +0700

    optimise seo

--- src/components/templates/signUpForm.js
+++ src/components/templates/signUpForm.js
@@ -22,33 +22,77 @@ const SignUpForm = props => {
   const password = useFormInput();
   const confirm = useFormInput();
 
-  const handleRegister = () => {
-    setState({ ...state, loading: true });
-    axios
-      .post("customer-register", {
-        name: name.value,
-        email: email.value,
-        phone: phone.value,
-        address: address.value,
-        password: password.value
-      })
-      .then(res => {
-        if (res.data.fail) {
-          setError({
-            alert: true,
-            message: res.data.fail
-          });
-        } else {
-          setState({
-            ...state,
-            isSuccess: true,
-            loading: false
-          });
-        }
-      })
-      .catch(err => {
-        setState({ ...state, loading: false });
+  const isValid = () => {
+    if (name.value === "") {
+      setError({
+        alert: true,
+        message: "Họ tên không được bỏ trống"
       });
+      return false;
+    } else if (email.value === "") {
+      setError({
+        alert: true,
+        message: "Email không được bỏ trống"
+      });
+      return false;
+    } else if (phone.value === "") {
+      setError({
+        alert: true,
+        message: "Số điện thoại không được bỏ trống"
+      });
+      return false;
+    } else if (address.value === "") {
+      setError({
+        alert: true,
+        message: "Địa chỉ không được bỏ trống"
+      });
+      return false;
+    } else if (password.value.length < 8) {
+      setError({
+        alert: true,
+        message: "Mật khẩu phải dài hơn 8 ký tự"
+      });
+      return false;
+    } else if (password.value !== confirm.value) {
+      setError({
+        alert: true,
+        message: "Mật khẩu xác nhận không trùng khớp"
+      });
+      return false;
+    }
+    return true;
+  };
+
+  const handleRegister = () => {
+    if (isValid()) {
+      setState({ ...state, loading: true });
+      axios
+        .post("customer-register", {
+          name: name.value,
+          email: email.value,
+          phone: phone.value,
+          address: address.value,
+          password: password.value
+        })
+        .then(res => {
+          if (res.data.fail) {
+            setError({
+              alert: true,
+              message: res.data.fail
+            });
+            setState({ ...state, loading: false });
+          } else {
+            setState({
+              ...state,
+              isSuccess: true,
+              loading: false
+            });
+          }
+        })
+        .catch(err => {
+          setState({ ...state, loading: false });
+        });
+    }
   };
 
   return state.isSuccess ? (
